1202f92e2dae561523f1f28f9353b64394570c6e
Carl Bergquist <carl.bergquist@gmail.com>
2013-11-05 18:34:47 +0100

    almost there! AIRPLANE COMMIT!

--- tasks/usecdn.js
+++ tasks/usecdn.js
@@ -1,7 +1,12 @@
 'use strict';
 
 module.exports = function(grunt) {
-  grunt.registerMultiTask('use-cdn', '', function() {
+  grunt.registerTask('use-cdn', '', function() {
 
+    console.log('USE CDN GOT DAMNIT!')
+    if (!(grunt.filerev || grunt.filerev.summary)) {
+
+      grunt.fail.fatal('missing grunt.filerev.summary. Make sure you run grunt-filerev before this')
+    }
   });
 }
--- tests/test-requirement.js
+++ tests/test-requirement.js
@@ -1,8 +1,41 @@
 'use strict';
-var assert = require('assert');
+var assert = require('assert'),
+  grunt = null
 
-describe('first test', function() {
-  it('is failing', function() {
-    assert.equal(false, true)
+describe('use cdn grunt task', function() {
+  beforeEach(function() {
+    grunt = require('grunt');
+    grunt.task.init([])
+    grunt.config.init({})
+    grunt.task.loadTasks('tasks')
+  })
+
+  describe('without grunt.filerev.summary', function() {
+    beforeEach(function() {
+      grunt.filerev = {
+        summary: undefined
+      }
+    })
+
+    it('should fail', function() {
+      grunt.task.run('use-cdn')
+      assert.equal(grunt.fail.errorcount, 1)
+    })
+  })
+
+  describe('with grunt.filerev.summary', function() {
+    beforeEach(function() {
+      grunt.filerev = {
+        summary: {
+        'image.png': 'image.t5fvco4.png',
+        'script.js': 'script.7jeoe.png'
+        }
+      }
+    })
+
+    it('should work just fine', function() {
+      grunt.task.run('use-cdn')
+      assert.equal(grunt.fail.errorcount, 0)
+    })
   })
 })
