61798809dd1db52569adc0a691434bda4863ef84
Carl Bergquist <carl.bergquist@gmail.com>
2013-11-08 13:37:37 +0100

    style: improve test names

--- tests/test-replacer.js
+++ tests/test-replacer.js
@@ -3,19 +3,29 @@ var assert = require('assert')
 var replacer = require('../libs/replacer').create('//static.com')
 
 describe('replacer', function() {
-  before(function() {
-    this.summary = {
-      '/static/image.png': '/static/image.2awfa3.png',
-      'static/script.js': 'static/script.fqwert.js'
-    }
-    this.result = replacer.prefix(this.summary)
+  describe('with relative paths', function() {
+    before(function() {
+      this.summary = {
+        '/static/image.png': '/static/image.2awfa3.png'
+      }
+      this.result = replacer.prefix(this.summary)
+    })
+
+    it('image.png should have cdn', function() {
+      assert.equal(this.result['/static/image.png'], '//static.com/static/image.2awfa3.png')
+    })
   })
 
-  it('image.png should have cdn', function() {
-    assert.equal(this.result['/static/image.png'], '//static.com/static/image.2awfa3.png')
-  })
+  describe('with static paths', function() {
+    before(function() {
+      this.summary = {
+        'static/script.js': 'static/script.fqwert.js'
+      }
+      this.result = replacer.prefix(this.summary)
+    })
 
-  it('script.js should have cdn', function() {
-    assert.equal(this.result['static/script.js'], '//static.com/static/script.fqwert.js')
+    it('script.js should have cdn', function() {
+      assert.equal(this.result['static/script.js'], '//static.com/static/script.fqwert.js')
+    })
   })
 })
